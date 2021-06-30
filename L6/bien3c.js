1b9ac736f86a37e98dfb1c0f70ae657a60a4566b
Le Phu Hieu <hieulephu3007@gmail.com>
2021-06-30 09:50:43 +0700

    upbai

---
+++ L6/bien3c.js
@@ -0,0 +1,7 @@
+function chia() {
+    let n1 = parseInt(document.getElementById('sa').value);
+    console.log(sa);
+    let n2 = parseInt(document.getElementById('sb').value);
+    console.log(sb);
+    let kt = n1 % n2
+}
